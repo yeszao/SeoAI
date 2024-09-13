b623fff6fccceed9f46c7d233f1856a5a76c607b
Guozao Meng <guozao.m@gmail.com>
2024-09-13 17:27:13 +0400

    Optimize prompt

--- lib/openai_utils.py
+++ lib/openai_utils.py
@@ -18,8 +18,9 @@ def generate_seo_content(html, keywords) -> SeoResult:
         {"role": "system", "content": "You are a Google SEO expert."},
         {"role": "assistant", "content": f"The current product detail html is: \n{html}"},
         {"role": "assistant", "content": f"The SEO keywords we want to use are: \n{keywords}"},
-        {"role": "user", "content": "Generate me a revised version of the detailed html, correcting any syntax errors, fixing incoherent sentences, and friendly to SEO principles."},
-        {"role": "user", "content": "And a short SEO-friendly description within 200 chars."},
+        {"role": "user", "content": "Generate a revised version of the detailed html, correcting any syntax errors, fixing incoherent sentences, and friendly to SEO principles."},
+        {"role": "user", "content": "And generate a short SEO-friendly description within 200 chars."},
+        {"role": "user", "content": "Remove font-family for html tags and remove style if it is empty."},
         {"role": "user", "content": "The output should be in pure JSON dictionary with two keys: 'revised_html' and 'short_description'."},
     ]
 
